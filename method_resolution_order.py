620c3f9109487f7b9ee3db76b047726490b2d14d
buyinzasamuel <buyinza190@gmail.com>
2025-06-03 15:51:42 +0300

    Assignment 3

---
+++ method_resolution_order.py
@@ -0,0 +1,21 @@
+class A:
+    def greet(self):
+        return "Hello from A"
+
+class B(A):
+    def greet(self):
+        return "Hello from B"
+
+class C(A):
+    def greet(self):
+        return "Hello from C"
+
+class D(B, C):
+    pass
+
+# Usage
+d = D()
+print(d.greet())  # Output: Hello from B
+
+# Check the Method Resolution Order
+print(D.mro())  # Output: [<class '__main__.D'>, <class '__main__.B'>, <class '__main__.C'>, <class '__main__.A'>, <class 'object'>]
