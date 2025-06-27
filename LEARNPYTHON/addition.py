7ed79e33fd2659e9b205a6866491bedaa9661f51
buyinzasamuel <buyinza365@gmail.com>
2025-06-27 11:51:25 +0300

    Add files via upload

---
+++ LEARNPYTHON/addition.py
@@ -0,0 +1,6 @@
+def sum(x,y): return x+y
+print("the sum is:", sum(10,20))
+def subtract(x,y): return y-x
+print("the subtyraction of the two values is:",subtract(10,20))
+def multiply(x,y): return x*y
+print("the multiplied value is:",multiply(10,20))---
---
+++ LEARNPYTHON/app.py
@@ -0,0 +1,1 @@
+print("Python has become too interesting")
